7139d582ea8f4a88846c87c29bb3263dbdb8b83a
zeeshan580 <106875023+zeeshan580@users.noreply.github.com>
2022-09-27 12:15:47 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -9,5 +9,5 @@ def newDeploy(ip,aapname)
 {
   
   
-  sh 'scp /home/ubuntu/.jenkins/workspace/declarative pipeline/webapp/target/webapp.war ubuntu@"${ip}":/var/lib/tomcat9/webapps/"${appname}".war'
+  sh "scp /home/ubuntu/.jenkins/workspace/declarative pipeline/webapp/target/webapp.war ubuntu@"${ip}":/var/lib/tomcat9/webapps/"${appname}".war"
 }
